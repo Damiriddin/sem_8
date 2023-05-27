a0c22193b4bb19af1ca3b341c48fc325c398d352
Damiriddin <128897262+Damiriddin@users.noreply.github.com>
2023-05-27 16:35:57 +0300

    Update main.c

--- sem8z2/main.c
+++ sem8z2/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-    void sort(char **arr, int left, int right) {
-        int i = left, j = right;
-        char *mid = arr[(left + right) / 2];
-        while (i <= j) {
-            while (strlen(arr[i]) < strlen(mid))
+// реализация быстрой сортировки для длин строк 
+void qsort(char **arr, int left, int right) {
+    int i = left, j = right;
+    char *mid = arr[(left + right) / 2];
+    while (i <= j) {
+        while (strlen(arr[i]) < strlen(mid))
                 i++;
         while (strlen(arr[j]) > strlen(mid))
             j--;
@@ -15,28 +16,33 @@
             arr[j] = temp;
             i++;
             j--;
-    }
         }
+    }
     if (left < j)
-        sort(arr, left, j);
+        qsort(arr, left, j);
     if (i < right)
-        sort(arr, i, right);
+        qsort(arr, i, right);
 }
 
 int main() {
+ 
     int n;
     printf("Enter number of strings: ");
-    scanf("%d", &n);
+    scanf("%d", &n);   //  считываем количество вводимых строк
     char **arr = (char **) malloc(n * sizeof(char *));
+   
     printf("Enter %d strings:\n", n);
+    // выделяем память под каждую строку
     for (int i = 0; i < n; i++) {
-    char *str = (char *) malloc(256 * sizeof(char));
+        char *str = (char *) malloc(256 * sizeof(char));
     scanf("%s", str);
-    arr[i] = str;
+    arr[i] = str; // заносим указатель на строку в массив
     }
-    sort(arr, 0, n - 1);
+    qsort(arr, 0, n - 1);
     printf("Sorted strings:\n");
     for (int i = 0; i < n; i++)
-    printf("%s\n", arr[i]);
+        printf("%s\n", arr[i]);
+
+    
     return 0;
 }
